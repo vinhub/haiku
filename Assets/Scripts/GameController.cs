f743a0fe8266517ee3acd861fc38cfaab6032914
vinhub <vin.bhalerao@gmail.com>
2019-09-10 22:53:55 -0700

    load skybox from url

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,52 +1,83 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
+
+[Serializable]
+public struct VistaData
+{
+    public string fileName;
+}
+
+[Serializable]
+public struct VistaDatas
+{
+    public VistaData[] vistaDatas;
+}
 
 public class GameController : MonoBehaviour
 {
-    int iMaterialCur = 0;
-    List<Material> materials = new List<Material>();
+    int iVistaCur = 0;
+    VistaData[] vistas;
 
     void Start()
     {
-        //if (materials.Count() == 0)
-        //{
-        //    IEnumerable<string> paths = AssetDatabase.FindAssets(null, new[] { "Assets/Materials/Skyboxes" }).Select(guid => AssetDatabase.GUIDToAssetPath(guid));
-
-        //    foreach (string path in paths)
-        //    {
-        //        Material material = (Material)AssetDatabase.LoadMainAssetAtPath(path); // new Material(RenderSettings.skybox);
-        //        materials.Add(material);
-        //    }
-
-        //    RenderSettings.skybox = materials[iMaterialCur];
-        //    DynamicGI.UpdateEnvironment();
-        //}
-
+        TextAsset jsonTextFile = Resources.Load<TextAsset>("vistas");
+        VistaDatas vistaDatas = JsonUtility.FromJson<VistaDatas>(jsonTextFile.text);
+        vistas = vistaDatas.vistaDatas;
     }
 
     public void NextVista()
     {
-        //iMaterialCur = (iMaterialCur + 1) % materials.Count();
-
-        //if (RenderSettings.skybox.name != materials[iMaterialCur].name)
-        //{
-        //    RenderSettings.skybox = materials[iMaterialCur];
-        //    DynamicGI.UpdateEnvironment();
-        //}
+        iVistaCur = (iVistaCur + 1) % vistas.Count();
+        if (RenderSettings.skybox.name != vistas[iVistaCur].fileName)
+        {
+            loadVista(iVistaCur);
+        }
     }
 
     public void PrevVista()
     {
-        //iMaterialCur = ((iMaterialCur - 1) >= 0) ? (iMaterialCur - 1) : (materials.Count() - 1);
+        iVistaCur = ((iVistaCur - 1) >= 0) ? (iVistaCur - 1) : (vistas.Count() - 1);
 
-        //if (RenderSettings.skybox.name != materials[iMaterialCur].name)
-        //{
-        //    RenderSettings.skybox = materials[iMaterialCur];
-        //    DynamicGI.UpdateEnvironment();
-        //}
+        if (RenderSettings.skybox.name != vistas[iVistaCur].fileName)
+        {
+            loadVista(iVistaCur);
+        }
+    }
+
+    private void loadVista(int iVista)
+    {
+        StartCoroutine(loadImage(iVista));
+    }
+
+    private IEnumerator loadImage(int iVista)
+    {
+        Uri uri = new Uri(Application.streamingAssetsPath + "/Skyboxes/" + vistas[iVista].fileName);
+
+        using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(uri))
+        {
+            yield return uwr.SendWebRequest();
+
+            if (uwr.isNetworkError || uwr.isHttpError)
+            {
+                Debug.Log(uwr.error);
+            }
+            else
+            {
+                // Get downloaded asset bundle
+                Texture2D texture = DownloadHandlerTexture.GetContent(uwr);
+                Material matSkybox = new Material(Shader.Find("Standard"));
+                matSkybox.SetTexture("_MainTex", texture);
+                RenderSettings.skybox = matSkybox;
+                DynamicGI.UpdateEnvironment();
+            }
+        }
     }
 }
