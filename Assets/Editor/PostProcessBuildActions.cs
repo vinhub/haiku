b7f6d7946b92f9687f3eed71fb1f25bec5373644
vinhub <vin.bhalerao@gmail.com>
2019-09-18 21:51:46 -0700

    fix web deployment issues

---
+++ Assets/Editor/PostProcessBuildActions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEditor.Callbacks;
+     
+public class PostBuildActions
+{
+    [PostProcessBuild(1)]
+    public static void OnPostProcessBuild(BuildTarget target, string targetPath)
+    {
+        // remove mobile warning
+        string path = Path.Combine(targetPath, "Build/UnityLoader.js");
+        string text = File.ReadAllText(path);
+        text = text.Replace("UnityLoader.SystemInfo.mobile", "false");
+        File.WriteAllText(path, text);
+
+        // copy web.config
+        string srcPath = Path.GetFullPath(Path.Combine(targetPath, "../Deploy/web.config"));
+        string dstPath = Path.Combine(targetPath, "web.config");
+        File.Copy(srcPath, dstPath, true);
+    }
+}
